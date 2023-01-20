4d9df659c54f5593b2cf9b1de242e9b27d624c38
MauricioDiias <mauriciod664@gmail.com>
2023-01-20 17:51:32 -0300

    one table

---
+++ tabela_antd/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { Menu } from './Menu';
+import { Table } from 'antd';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Menu</h1>
+        <Menu/>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ tabela_antd/src/data.js
@@ -0,0 +1,37 @@
+export const menu = [
+    {
+        "id": "1",
+        "name": "Steak Frites",
+        "description": "Bife grelhado acompanhado de batatas fritas",
+        "price": "22.99",
+        "attributes": ["carne", "batatas fritas", "grelhado"]
+    },
+    {
+        "id": "2",
+        "name": "Poulet à la crème",
+        "description": "Frango em molho de creme e ervas",
+        "price": "18.99",
+        "attributes": ["frango", "creme", "ervas"]
+    },
+    {
+        "id": "3",
+        "name": "Sushi Maki",
+        "description": "Conjunto de makis de peixe e vegetais",
+        "price": "15.99",
+        "attributes": ["peixe", "vegetais", "sushi"]
+    },
+    {
+        "id": "4",
+        "name": "Paella Valenciana",
+        "description": "Paella de frutos do mar e arroz",
+        "price": "21.99",
+        "attributes": ["frutos do mar", "arroz"]
+    },
+    {
+        "id": "5",
+        "name": "Gnocchi al Pesto",
+        "description": "Gnocchi em molho de pesto de manjericão",
+        "price": "16.99",
+        "attributes": ["gnocchi", "pesto", "manjericão"]
+    }
+]
